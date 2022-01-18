52518bc4f978e8311e4f51b1c7cc98cb7abf5e86
Teyu <a0924xxx@gmail.com>
2022-01-18 18:29:23 +0800

    try

---
+++ app/src/main/java/go/deyu/myapplication/di/AppModule.kt
@@ -0,0 +1,25 @@
+package go.deyu.myapplication.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Singleton
+
+
+/**
+ * Module to tell Hilt how to provide instances of types that cannot be constructor-injected.
+ *
+ * As these types are scoped to the application lifecycle using @Singleton, they're installed
+ * in Hilt's ApplicationComponent.
+ */
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideIoDispatcher() = Dispatchers.IO
+
+}
